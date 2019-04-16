8a669d4928993b06a4dfb1ea87bd8c9b99c8618d
Alice green <alicemary2011@gmail.com>
2019-04-16 11:09:59 -0400

    Site updated to 34e7eeb

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,33 @@
-// This is where it all goes :)
+// main menu scrolls to different sections
+
+// node list for main menu buttons
+const MAINMENU = document.querySelectorAll(".dropdown button");
+
+// dropdown menu
+const DROPDOWN = document.querySelector(".dropdown");
+
+// hamburger icon
+const HAMBURGER = document.querySelector(".hamburger_container");
+
+
+
+// main menu buttons scroll to different sections on the webpage
+for (let button of MAINMENU){
+	button.addEventListener('click', function(){
+		let section = button.innerHTML;
+		console.log(section);
+		document.getElementById(section).scrollIntoView({behavior: "smooth"});
+	 	console.log("button clicked");
+	});
+};
+
+// hide the dropdown menu when the page loads
+DROPDOWN.classList.add("hide");
+
+function toggleMenu(){
+	console.log("toggle");
+	DROPDOWN.classList.toggle("hide");
+}
+
+
+HAMBURGER.addEventListener('click', toggleMenu);
